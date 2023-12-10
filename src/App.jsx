c7c5923921822fa4297c6b8245c47c8de4b9b216
Julio Ucharima <julio_ucharima@usmp.pe>
2023-12-09 23:25:14 -0500

    feat: update client search UI and integrate with API

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,74 @@
-import "./App.css";
+import { useEffect, useState } from "react";
 import { FaSearch } from "react-icons/fa";
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState([]);
+  const [nombreCliente, setNombreCliente] = useState("");
+
+  useEffect(() => {
+    if (nombreCliente) {
+      const fetchData = async () => {
+        const response = await fetch(
+          `http://127.0.0.1:8000/api/clientes/${nombreCliente}/equipos-alquilados`
+        );
+        const json = await response.json();
+        setData(json);
+      };
+
+      fetchData();
+    }
+  }, [nombreCliente]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setNombreCliente(event.target.elements.search.value);
+  };
+
   return (
     <>
       <h1>Consulta de clientes</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="container-search">
           <span className="icon">
             <FaSearch />
           </span>
-          <input type="search" name="search" placeholder="Empresa 1" />
+          <input
+            type="search"
+            name="search"
+            placeholder="Empresa 1, Empresa 2, ..."
+          />
         </div>
 
         <button>Buscar</button>
       </form>
 
-      <table className="rwd-table">
-        <thead>
-          <tr>
-            <th>Código</th>
-            <th>Tipo de UA</th>
-            <th>Marca</th>
-            <th>Procesador</th>
-            <th>Generación</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr key="1">
-            <td>Ejemplo</td>
-            <td>Ejemplo</td>
-            <td>Ejemplo</td>
-            <td>Ejemplo</td>
-            <td>Ejemplo</td>
-          </tr>
-        </tbody>
-      </table>
+      {data && data.length > 0 ? (
+        <table className="rwd-table">
+          <thead>
+            <tr>
+              <th>Código</th>
+              <th>Tipo de UA</th>
+              <th>Marca</th>
+              <th>Procesador</th>
+              <th>Generación</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((equipo) => (
+              <tr key={equipo.ID_Equipos_Alquilados}>
+                <td>{equipo.Codigo_Unidad_Alquiler}</td>
+                <td>{equipo.TipoDeUA}</td>
+                <td>{equipo.Marca}</td>
+                <td>{equipo.Procesador}</td>
+                <td>{equipo.Generacion}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <p>No se encontraron resultados</p>
+      )}
     </>
   );
 }
